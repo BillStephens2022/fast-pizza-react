f1ac7b5a1e5c31fbeb7d69ac633d800503935d38
BillStephens2022 <two4onebill@yahoo.com>
2024-07-26 06:59:01 -0400

    updated styling on home page, header, cart overview, create user components

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,11 +3,13 @@ import SearchOrder from "../features/order/SearchOrder";
 import Username from "../features/user/Username";
 
 export default function Header() {
-    return (
-        <header className="bg-yellow-500 uppercase">
-            <Link to="/" className="tracking-widest">Fast React Pizza Co.</Link>
-            <SearchOrder />
-            <Username />
-        </header>
-    );
+  return (
+    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
+      <SearchOrder />
+      <Username />
+    </header>
+  );
 }
